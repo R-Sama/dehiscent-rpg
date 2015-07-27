870b1fed3d9e7e719a62e014def831399ef9e970
Sinner-Richu <richardxiao.liu@gmail.com>
2015-07-27 20:35:27 +0100

    Remove helper function for IO.formatColumns() to try and reduce possibility for poor compilers to fault on ambiguity (no subtype between boolean and string arguments means they can't find a 'more precise' function

---
+++ core/Quest.java
@@ -0,0 +1,17 @@
+package core;
+
+import java.util.Map;
+
+/**
+ * A class to handle logging of active/completed quests received by the Player.
+ * Contains all methods to update/maintain/return values for quests.
+ */
+public class Quest {
+
+    private int questNumber;
+    private String questName, questDescription;
+    private String questRequirement;
+    private Map<String, String> requirementList;
+
+
+}
